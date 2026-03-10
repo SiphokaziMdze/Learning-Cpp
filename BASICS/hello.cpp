2b1f31074d5e4b001e04b070ebeac48e24cf630e
SIPHOKAZI VERONICA MDZE <112616851+SiphokaziMdze@users.noreply.github.com>
2026-03-10 14:31:03 +0200

    first hello world passoword.

---
+++ BASICS/hello.cpp
@@ -0,0 +1,7 @@
+#include <iostream> //tells the compiler to include the iostream library, which allows us to use input and output functions like std::cout
+
+int main() {//the main function is the entry point of the program, where execution starts
+    std::cout << "Hello Siphokazi! Your C++ setup is working correctly." << std::endl; //std::cout is used to print the message to the console, and *std::endl* adds a new line after the message
+    std::cout <<"Hello, my name is Siphokazi Mdze and I am learning C++ programming language." << std::endl; //prints another message to the console
+    return 0;//return 0 indicates that the program ended successfully
+}
