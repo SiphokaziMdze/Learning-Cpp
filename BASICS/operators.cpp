1346e294ca382e70cfc936675a66b22bbea98497
SIPHOKAZI VERONICA MDZE <112616851+SiphokaziMdze@users.noreply.github.com>
2026-03-12 14:50:19 +0200

    calculations

--- BASICS/operators.cpp
+++ BASICS/operators.cpp
@@ -5,12 +5,16 @@ int main (){
     int a;
     int b;
 
-    std::cout << "Enter the first int: " << a << std::endl;
+    std::cout << "Enter the first int: " << std::endl;
     std::cin >> a;
-    std::cout << "Enter the second int: " << b << std::endl;
+    std::cout << "Enter the second int: " << std::endl;
     std::cin >> b;
 
-    std::cout << "The sum of " << a << " and " << b << "is: " << a + b << std::endl; // Print the sum of a and b to the console
-    
+    std::cout << "The sum of " << a << " and " << b << " is: " << a + b << std::endl; // Print the sum of a and b to the console
+    std::cout << "The difference of " << a << " and " << b << " is: " << a - b << std::endl;
+    std::cout << "The product of " << a << " and " << b << " is: " << a * b << std::endl;
+    std::cout << "The division of " << a << " and " << b << " is: " << a / b << std::endl;
+    std::cout << "The remainder of " << a << " and " << b << " is: " << a % b << std::endl;
+
 
 }
