2c3669c8cd1086d2a69f1860bf56e78500cd3c86
SIPHOKAZI VERONICA MDZE <112616851+SiphokaziMdze@users.noreply.github.com>
2026-03-20 15:29:16 +0200

    finding max value

--- LeetCodeExercises/Easy/Task1.cpp
+++ LeetCodeExercises/Easy/Task1.cpp
@@ -1,7 +1,27 @@
+#include <iostream>
 /*
 Write a function:
 int findMax(int arr[], int size);
 Return the largest number
 */
 
-int m
+int findMax(int arr[], int size){
+
+    int max = arr[0];
+
+    for(int i = 1; i < size; i++){
+        if(arr[i] > max){
+            max = arr[i];
+        }
+    }
+    return max;
+}
+
+int main() {
+
+    int arr[5] = {89, 65, 47, 3, 23};
+
+    std::cout << "Max value = " << findMax(arr, 5);
+
+    return 0;
+}
